0b1be239d93b01e66f1a7a17a64709893271f201
Joko Sarjono Slameto <johno.smakaduta@gmail.com>
2019-03-07 23:09:45 +0700

    Add GridView

--- app/src/main/java/id/ac/mercubuana/joko_ss/myrecyclerview/MainActivity.java
+++ app/src/main/java/id/ac/mercubuana/joko_ss/myrecyclerview/MainActivity.java
@@ -2,8 +2,11 @@ package id.ac.mercubuana.joko_ss.myrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -28,4 +31,35 @@ public class MainActivity extends AppCompatActivity {
         listPresidentAdapter.setListPresident(list);
         rvCategory.setAdapter(listPresidentAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_list:
+                showRecyclerList();
+                break;
+
+            case R.id.action_grid:
+                showRecyclerGrid();
+                break;
+
+            case R.id.action_cardview:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showRecyclerGrid(){
+        rvCategory.setLayoutManager(new GridLayoutManager(this, 2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
+    }
+
 }
